f40eaa05ef41c4c64c8ea6acbda546c4b2d587fd
Paulo M Fernandez <paulofer85@gmail.com>
2023-06-05 17:09:59 -0300

    update libs using v2

--- Runtime/QuartersSDK/Scripts/ConfigurationSettings/ApiParamsSettings.cs
+++ Runtime/QuartersSDK/Scripts/ConfigurationSettings/ApiParamsSettings.cs
@@ -15,7 +15,7 @@ namespace Packages.gg.poq.unity.sdk.Runtime.QuartersSDK.Scripts.ConfigurationSet
             Settings.Add("DASHBOARD_URL", "https://www.poq.gg/dev");
             Settings.Add("BASE_URL", "https://www.poq.gg");
             Settings.Add("AVATAR_URL", "https://www.poq.gg/images/");
-            Settings.Add("API_ENDPOINT", "/api/v2");
+            Settings.Add("API_ENDPOINT", "/api/v2/oauth2");
             Settings.Add("BUY_ENDPOINT", "/buy");
             Settings.Add("AUTHORIZE_ENDPOINT", "/oauth2/authorize");
             Settings.Add("USER_DETAILS_ENDPOINT", "/users/me");
